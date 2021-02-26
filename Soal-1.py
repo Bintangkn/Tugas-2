e9237719696041e38afa171c0426411d8e6ce50c
Bintangkn <78997112+Bintangkn@users.noreply.github.com>
2021-02-26 13:12:58 +0700

    Add files via upload

---
+++ Soal-1.py
@@ -0,0 +1,13 @@
+kontak = []
+
+def daftar():
+    for x in kontak:
+        print("nama: {}" .format(x["nama"]))
+        print("no. telepon: {}" .format(x["no. telepon"]))
+
+def tambah():
+    x = {}
+    nama = input("nama: ")
+    no.telepon = input("no. telepon")
+    x["nama"]=nama
+    x["no. telepon"] = telepon
